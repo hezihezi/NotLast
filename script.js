5e847a8fb9fb5ff5a3833e730ab5478201f2a5f8
hezihezi <hj2393@columbia.edu>
2015-03-01 13:33:51 -0500

    added a title to the hAxis and the gridlines still do not work in the line chart

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+	  horizontalTitle = {title: 'dates',  titleTextStyle: {color: '#FF0000'}}
+	  myGridlines = {color: '#FF0000', count: '8'}
+	  
+	  myMinor = {}
+	  
 	  // Load the Visualization API and the piechart package.
       google.load('visualization', '1.0', {'packages':['corechart']});
 
@@ -32,11 +38,17 @@
         data.addRows(bigArray);
 
         // Set chart options
-        var options = {'title':'How Much Pizza Did Seres Eat Over the Years',
-                       'width':1000,
-                       'height':800};
+        var options = {'title':'THIS ASSIGNMENT IS HARD',
+                       'width':1200,
+                       'height':800,
+                       'hAxis':{}
+                      };
+                      
+		options.hAxis = horizontalTitle; 
+		options.hAxis.gridlines = myGridlines;
+		
 
         // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
         chart.draw(data, options);
       }
